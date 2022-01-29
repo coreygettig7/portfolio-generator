9f9d71af6ec3f9dc1e1d909b1dedb31ed7673fef
Corey Gettig <coreygettig7@gmail.com>
2022-01-29 13:19:29 -0500

    added ability to generate index.html page with user input

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-  
-printProfileData(profileDataArgs);
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw (err);
+    
+    console.log('Portfolio complete! Check out index.html to see the output!')
+}); 
